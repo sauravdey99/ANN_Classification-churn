d5524199e3aa8c607af4cc54a61706a12d9302d0
sauravdey99 <77602664+sauravdey99@users.noreply.github.com>
2026-02-27 10:26:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ with col1:
 
 with col2:
     credit_score     = st.number_input('Credit Score',     min_value=300,  max_value=850,   value=600)
-    balance          = st.number_input('Balance',          min_value=0.0,                   value=50000.0)
-    estimated_salary = st.number_input('Estimated Salary', min_value=0.0,                   value=50000.0)
+    balance          = st.slider('Balance',          min_value=0.0,                   value=50000.0)
+    estimated_salary = st.slider('Estimated Salary', min_value=0.0,                   value=50000.0)
     has_cr_card      = st.selectbox('Has Credit Card',     [0, 1])
     is_active_member = st.selectbox('Is Active Member',    [0, 1])
 
